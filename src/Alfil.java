2bb5c266af41af3bf3b1ef37e3d9aefd66ad8b89
JuanDiegoGutierrezDuenas <est.juand.gutierrezd@unimilitar.edu.co>
2026-03-24 10:00:50 -0500

    Clase de la ficha

--- src/Alfil.java
+++ src/Alfil.java
@@ -1,12 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+public class Alfil extends Pieza {
+    public Alfil(String color, int x, int y) {
+        super(color, "Alfil", x, y);
+    }
 
-/**
- *
- * @author juan2
- */
-public class Alfil {
-    
+    @Override
+    public boolean validarMovimiento(int nuevoX, int nuevoY, Pieza[][] tablero) {
+        return Math.abs(nuevoX - getPosicionX()) == Math.abs(nuevoY - getPosicionY());
+    }
 }
