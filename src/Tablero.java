badfd9eb3ed000b5377b8d949f301274c2d5dbbc
JuanDiegoGutierrezDuenas <est.juand.gutierrezd@unimilitar.edu.co>
2026-03-24 10:00:19 -0500

    Se modifico el tablero para que muestre el nombre de las fichas

--- src/Tablero.java
+++ src/Tablero.java
@@ -3,26 +3,57 @@ public class Tablero {
 
     public Tablero() {
         this.casillas = new Pieza[8][8];
-        inicializarPiezas(); // 👉 aquí se colocan las piezas
+        inicializarPiezas();
     }
 
     private void inicializarPiezas() {
-  
-        for (int j = 0; j < 8; j++) {
-            casillas[6][j] = new Peon("blanco", 6, j);
+    // Peones
+    for (int j = 0; j < 8; j++) {
+        casillas[6][j] = new Peon("blanco", 6, j);
+        casillas[1][j] = new Peon("negro", 1, j);
+    }
+
+    // Torres
+    casillas[7][0] = new Torre("blanco", 7, 0);
+    casillas[7][7] = new Torre("blanco", 7, 7);
+    casillas[0][0] = new Torre("negro", 0, 0);
+    casillas[0][7] = new Torre("negro", 0, 7);
+
+    // Caballos
+    casillas[7][1] = new Caballo("blanco", 7, 1);
+    casillas[7][6] = new Caballo("blanco", 7, 6);
+    casillas[0][1] = new Caballo("negro", 0, 1);
+    casillas[0][6] = new Caballo("negro", 0, 6);
+
+    // Alfiles
+    casillas[7][2] = new Alfil("blanco", 7, 2);
+    casillas[7][5] = new Alfil("blanco", 7, 5);
+    casillas[0][2] = new Alfil("negro", 0, 2);
+    casillas[0][5] = new Alfil("negro", 0, 5);
+
+    // Reina y Rey
+    casillas[7][3] = new Reina("blanco", 7, 3);
+    casillas[7][4] = new Rey("blanco", 7, 4);
+    casillas[0][3] = new Reina("negro", 0, 3);
+    casillas[0][4] = new Rey("negro", 0, 4);
+}
+
+
+    // 👉 Aquí va moverPieza
+    public void moverPieza(int filaOrig, int colOrig, int filaDest, int colDest) {
+        Pieza pieza = casillas[filaOrig][colOrig];
+        if (pieza == null) {
+            System.out.println("No hay pieza en la posición de origen.");
+            return;
         }
-
-        for (int j = 0; j < 8; j++) {
-            casillas[1][j] = new Peon("negro", 1, j);
+        if (pieza.validarMovimiento(filaDest, colDest, casillas)) {
+            pieza.setPosicion(filaDest, colDest);
+            casillas[filaDest][colDest] = pieza;
+            casillas[filaOrig][colOrig] = null;
+            System.out.println("Movimiento realizado.");
+        } else {
+            System.out.println("Movimiento inválido para " + pieza.getTipo());
         }
-
-   
-        casillas[7][0] = new Torre("blanco", 7, 0);
-        casillas[7][7] = new Torre("blanco", 7, 7);
-        casillas[0][0] = new Torre("negro", 0, 0);
-        casillas[0][7] = new Torre("negro", 0, 7);
-
-        // ... y así con caballos, alfiles, reina y rey
     }
 
     public void mostrarTablero() {
@@ -38,4 +69,5 @@ public class Tablero {
         }
     }
 }
+
 
