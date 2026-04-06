88503e3c2390a9b12af754ed1bed27c1a5cf38a6
JuanDiegoGutierrezDuenas <est.juand.gutierrezd@unimilitar.edu.co>
2026-04-06 18:21:45 -0500

    Se edita la ficha para que no traspase a las demas

--- src/Alfil.java
+++ src/Alfil.java
@@ -4,7 +4,30 @@ public class Alfil extends Pieza {
     }
 
     @Override
-    public boolean validarMovimiento(int nuevoX, int nuevoY, Pieza[][] tablero) {
-        return Math.abs(nuevoX - getPosicionX()) == Math.abs(nuevoY - getPosicionY());
+public boolean validarMovimiento(int nuevoX, int nuevoY, Pieza[][] tablero) {
+    int dx = nuevoX - getPosicionX();
+    int dy = nuevoY - getPosicionY();
+
+
+    if (Math.abs(dx) != Math.abs(dy)) {
+        return false;
     }
+
+    int stepX = Integer.compare(dx, 0);
+    int stepY = Integer.compare(dy, 0);
+
+    int x = getPosicionX() + stepX;
+    int y = getPosicionY() + stepY;
+
+    while (x != nuevoX && y != nuevoY) {
+        if (tablero[x][y] != null) {
+            return false; 
+        }
+        x += stepX;
+        y += stepY;
+    }
+
+    return tablero[nuevoX][nuevoY] == null ||
+           !tablero[nuevoX][nuevoY].getColor().equals(this.getColor());
+ }
 }
