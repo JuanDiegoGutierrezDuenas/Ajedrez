db1daab6c96319251761e50b34452f1718341392
JuanDiegoGutierrezDuenas <est.juand.gutierrezd@unimilitar.edu.co>
2026-04-06 18:26:34 -0500

    Se edita la ficha para que no traspase a las demas

--- src/Reina.java
+++ src/Reina.java
@@ -4,8 +4,31 @@ public class Reina extends Pieza {
     }
 
     @Override
-    public boolean validarMovimiento(int nuevoX, int nuevoY, Pieza[][] tablero) {
-        return (nuevoX == getPosicionX() || nuevoY == getPosicionY()) ||
-               (Math.abs(nuevoX - getPosicionX()) == Math.abs(nuevoY - getPosicionY()));
+public boolean validarMovimiento(int nuevoX, int nuevoY, Pieza[][] tablero) {
+    int dx = nuevoX - getPosicionX();
+    int dy = nuevoY - getPosicionY();
+
+  
+    if (!(nuevoX == getPosicionX() || nuevoY == getPosicionY() || Math.abs(dx) == Math.abs(dy))) {
+        return false;
     }
+
+    int stepX = Integer.compare(dx, 0);
+    int stepY = Integer.compare(dy, 0);
+
+    int x = getPosicionX() + stepX;
+    int y = getPosicionY() + stepY;
+
+    while (x != nuevoX || y != nuevoY) {
+        if (tablero[x][y] != null) {
+            return false; 
+        }
+        x += stepX;
+        y += stepY;
+    }
+
+    return tablero[nuevoX][nuevoY] == null ||
+           !tablero[nuevoX][nuevoY].getColor().equals(this.getColor());
+}
+
 }
